927bea4c21ecbc7c661a4c971d3ce25a26e53598
PieAreSquared11 <jbwpsa11@outlook.com>
2024-10-19 22:05:23 -0400

    Update home-page.js

--- home-page.js
+++ home-page.js
@@ -1,13 +1,11 @@
 class HomePage {
     run() {
-        const source = 'C:/Users/bensw/Desktop/read%20the%20structions';
-
         return [
             autoStyles([
                 'list'
             ]),
             
-            Heading('Read The Structions', '1', [
+            Heading('Your text', '1', [
                 Style({
                 'font-family': 'Climate Crisis'
                 })
